293f8b5b07594133f658fdf4eb2fddfb2c21dfae
Mogakamo <mogaka.amo254@gmail.com>
2021-04-04 18:13:54 +0300

    Commit

---
+++ wasify-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c25899928f0285ca2399582523cd7b6b'),
+    },
+  },
+});
---
+++ wasify-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f8c00a53-4a3a-4fdf-902c-2e36faabf835'
+};
