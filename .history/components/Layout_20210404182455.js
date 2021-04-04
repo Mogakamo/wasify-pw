b84fb48a0005c07111c37327e08feb1f0f7a6cb2
Mogakamo <mogaka.amo254@gmail.com>
2021-04-04 18:26:58 +0300

    Commit

---
+++ .history/components/Layout_20210404182455.js
@@ -0,0 +1,3 @@
+/** This is the layout of the website
+ *  - this component will depict how the homepage and also the design of the 
+ */
---
+++ .history/components/Layout_20210404182625.js
@@ -0,0 +1,11 @@
+/** This is the layout of the website
+ *  - this component will depict how the homepage and also the design of the 
+ *      other components how they will appear.
+ * NB: This will be for the sake of easier execution 
+ */
+
+const Layout = () => {
+
+}
+
+export default Layout;
