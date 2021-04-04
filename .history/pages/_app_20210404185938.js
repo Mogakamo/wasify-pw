b6ce965a7df497d6bd021fe474902ccbfb4a82f9
Mogakamo <mogaka.amo254@gmail.com>
2021-04-04 19:00:08 +0300

    Commit

--- .history/pages/_app_20210404185938.js
+++ .history/pages/_app_20210404185938.js
@@ -4,24 +4,6 @@ import NProgress from 'nprogress' //nprogress module
 import 'nprogress/nprogress.css'  //styles for the nprogress package
 import Head from 'next/head'
 
-const MyApp = ({
-    MainComponent,
-    pageProps,
-  }) => {
-    const router = useRouter();
-    const [pageLoading, setPageLoading] = React.useState < boolean > (false);
-    React.useEffect(() => {
-      const handleStart = () => {
-        setPageLoading(true);
-      };
-      const handleComplete = () => {
-        setPageLoading(false);
-      };
-
-      router.events.on('routeChangeStart', handleStart);
-      router.events.on('routeChangeComplete', handleComplete);
-      router.events.on('routeChangeError', handleComplete);
-    }, [router]);
 
 
 //Binding events for the loader
