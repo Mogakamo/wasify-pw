f9f660c5ad0e0104ccb02073b4d48e4e1a41117d
Mogakamo <mogaka.amo254@gmail.com>
2021-04-13 19:41:54 +0300

    changed the background image for the hero section

--- .history/components/Hero_20210413193929.js
+++ .history/components/Hero_20210413193929.js
@@ -5,7 +5,7 @@ const Hero = () => {
   <>
    <section className="bg-red-200 text-green-900 relative">
   {/*     <div class="min-h-screen hero-image bg-center bg-cover flex" style="background-image: url(https://images.unsplash.com/photo-1491438590914-bc09fcaaf77a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ);"> */}
-  <div className="min-h-screen hero-image bg-right-bottom bg-cover flex" style={{backgroundImage: 'url(https://source.unsplash.com/marketplace'}}>
+  <div className="min-h-screen hero-image bg-right-bottom bg-cover flex" style={{backgroundImage: 'url(https://sunsplash'}}>
     <div className="relative container mx-auto p-4 flex items-end z-10">
       <div>
         <div className="content float-left py-4 px-5 my-5">
