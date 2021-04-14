5873f7771c9d599c86db8954efaa3981e674e57a
Mogakamo <mogaka.amo254@gmail.com>
2021-04-14 10:49:32 +0300

    Commit

--- .history/components/SignUp-Form_20210414104835.js
+++ .history/components/SignUp-Form_20210414104835.js
@@ -23,7 +23,7 @@ const SignUpForm = () => {
             <div className="bg-grey-lighter min-h-screen flex flex-col">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-            <h1 className="mb-8 text-3xl text-center">Registration</h1>
+            <h1 className="mb-8 text-3xl text-center">Registration 🚀</h1>
             <input
               type="text"
               className="block border border-grey-light w-full p-3 rounded mb-4"
