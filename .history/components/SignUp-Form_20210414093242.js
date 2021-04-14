39aa69ae39c04129748daf1f9444d7972f533438
Mogakamo <mogaka.amo254@gmail.com>
2021-04-14 09:59:24 +0300

    Added the dashboard and profile pages

--- .history/components/SignUp-Form_20210414093242.js
+++ .history/components/SignUp-Form_20210414093242.js
@@ -1,12 +1,11 @@
 //sign up form
 import React from 'react'
 
-
 class SignUpForm extends React.Component {
     render() {
         return (
             <>
-             
+            
             </>
         )
     }
--- .history/components/SignUp-Form_20210414093246.js
+++ .history/components/SignUp-Form_20210414093246.js
@@ -1,7 +1,6 @@
 //sign up form
 import React from 'react'
 
-
 class SignUpForm extends React.Component {
     render() {
         return (
