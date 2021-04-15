317bae2c7928fe87587a19439818d802796f9067
Mogakamo <mogaka.amo254@gmail.com>
2021-04-15 14:34:55 +0300

    Commit

--- .history/next.config_20210415143446.js
+++ .history/next.config_20210415143446.js
@@ -19,5 +19,5 @@ module.exports = {
         SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
         EMAIL_FROM: process.env.EMAIL_FROM,
         SESSION_SECRET: process.env.SESSION_SECRET,
-    }
+    },
 };
