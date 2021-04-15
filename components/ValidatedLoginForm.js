141d1dc8db578d9aee1cc26cc42559e1f80d1fc9
Mogakamo <mogaka.amo254@gmail.com>
2021-04-15 16:28:31 +0300

    Commit

---
+++ components/ValidatedLoginForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class ValidatedLoginForm extends React.Component {
+	render() {
+	 return (
+	   <>
+	    <div>
+	    	<h1>Login</h1>
+
+
+	    </div>
+           </>
+	 )
+	}
+}
+
+export default ValidatedLoginForm
---
+++ pages/sign_in.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ValidatedLoginForm from '../components/ValidatedLoginForm'
+
+const SignIn = () => {
+	return (
+	 <>
+	  <ValidatedLoginForm />
+	 </>
+	)
+}
+
+export default SignIn
