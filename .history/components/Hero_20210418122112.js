eb52db0bb27dc34b24164fe19d4256a0fec51478
Mogakamo <mogaka.amo254@gmail.com>
2021-04-18 12:55:17 +0300

    Commit

--- .history/components/Hero_20210418122112.js
+++ .history/components/Hero_20210418122112.js
@@ -1,6 +1,6 @@
 /** Landing section and the top navigation */
 import React from 'react'
-
+import Typing from './'
 
 
 const Hero = () => {
@@ -12,8 +12,8 @@ const Hero = () => {
       <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
     </div>
     <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-        <br className="hidden lg:inline-block" />readymade gl
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+        <br className="hidden lg:inline-block" />readymade gluten
       </h1>
       <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
       <div className="flex justify-center">
