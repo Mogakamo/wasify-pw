f30fcf1a2379eaf2f3e70ec112d4cbb164e7bdd1
Mogakamo <mogaka.amo254@gmail.com>
2021-04-18 20:43:19 +0300

    Commit

---
+++ .history/pages/chat_20210418204116.js
@@ -0,0 +1,12 @@
+import React from 'react'
+imo
+
+const Chat = () => {
+        return (
+            <>
+
+            </>
+        )
+}
+
+export default Chat
---
+++ .history/pages/chat_20210418204118.js
@@ -0,0 +1,12 @@
+import React from 'react'
+impo
+
+const Chat = () => {
+        return (
+            <>
+
+            </>
+        )
+}
+
+export default Chat
