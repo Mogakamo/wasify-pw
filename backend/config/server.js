373cd797b88af842593e7c9cd36ec93a1980a134
Mogakamo <mogaka.amo254@gmail.com>
2021-04-19 22:08:09 +0300

    Added the backend with mongodb

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bddf51e4b464f406c9b64049b090553b'),
+    },
+  },
+});
