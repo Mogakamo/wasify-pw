dfcd194ab975f6b702bf3bfb7eb2a29c404df358
Mogakamo <mogaka.amo254@gmail.com>
2021-04-20 21:02:43 +0300

    commit

--- .history/components/HomePage_20210420210232.js
+++ .history/components/HomePage_20210420210232.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 class Home extends React.Component {
     render() {
-        return (
-            <>
-
-            </>
-        )
+        
     }
 }
--- .history/components/HomePage_20210420210237.js
+++ .history/components/HomePage_20210420210237.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
         return (
             <>
 
-            </>
+            
         )
     }
 }
