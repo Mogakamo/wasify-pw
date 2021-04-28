6d52edbbd434d263ae70a70af3ac4cb9f493b28d
Mogakamo <mogaka.amo254@gmail.com>
2021-04-28 17:45:16 +0300

    Added the chakra-ui

--- wasify-frontend/pages/_app.js
+++ wasify-frontend/pages/_app.js
@@ -1,12 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css'
 import { Provider } from "next-auth/client";
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core'
 
 function MyApp({ Component, pageProps }) {
   return (
+  <ThemeProvider theme={theme}>
     <Provider session={pageProps.session}>
+	  <CSSReset />
       <Component {...pageProps} />
     </Provider>
+  <ThemeProvider>
   );
 }
 
