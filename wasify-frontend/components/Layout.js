5bec0a6719e8736039bc890fd761e135d6b95fe9
Mogakamo <mogaka.amo254@gmail.com>
2021-04-28 19:17:51 +0300

    Added the Hero again

--- wasify-frontend/components/Layout.js
+++ wasify-frontend/components/Layout.js
@@ -1,24 +1,17 @@
 //Layout of the landing page 
 import React from 'react'
 import Footer from './Footer'
-import { Flex } from "@chakra-ui/react";
 import Hero from './Hero'
+import Footer from './Footer'
 
-const Layout = (props) => {
+const Layout = () => {
     return (
         <>
-	 <Flex 
-	    direction="column"
-	    align="center"
-	    maxW={{ xl: "1200px" }}
-	    m="0 auto"
-	    {...props}>
-
-	 </Flex>
-
 	<Hero />
         {/**Footer */}
-        </>
+        
+	 <Footer />
+	</>
     )
 }
 
