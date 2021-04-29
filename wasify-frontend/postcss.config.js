41bcb38d90cf1a1baa7ee4cae7507672731f4e76
Mogakamo <mogaka.amo254@gmail.com>
2021-04-29 09:41:35 +0300

    Commit

--- wasify-frontend/postcss.config.js
+++ wasify-frontend/postcss.config.js
@@ -5,6 +5,13 @@ module.exports = {
       autoprefixer: {
         flexbox: 'no-2009',
       },
+       (process.env.NODE_ENV === 'production') ? {
+	 '@fullhuman/postcss-purgecss': {
+                 // added sections folder and changed extension to jsx
+                 content: ['./components/**/*.js', './pages/**/*.js'],
+                 defaultExtractor: content =>
+                    content.match(/[\w-/:]+(?<!:)/g) || [], 
+       },
       stage: 3,
       features: {
         'custom-properties': false,
