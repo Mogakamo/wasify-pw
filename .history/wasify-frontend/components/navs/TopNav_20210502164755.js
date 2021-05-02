d4c7e82444827e77fdc02cf28bf8654ae397a25a
Mogakamo <mogaka.amo254@gmail.com>
2021-05-02 16:48:32 +0300

    commit

--- .history/wasify-frontend/components/navs/TopNav_20210502164755.js
+++ .history/wasify-frontend/components/navs/TopNav_20210502164755.js
@@ -4,7 +4,7 @@ const TopNav = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-      <div class="sticky top-0 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div class="sticky top-0 bg-red-9px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div class="relative flex items-center justify-between">
           <a
             href="/"
