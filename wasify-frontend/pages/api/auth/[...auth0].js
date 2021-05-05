c7a3760048dca937e921ec6ca44ded31d1dd3844
Mogakamo <mogaka.amo254@gmail.com>
2021-05-05 15:27:54 +0300

    commit

---
+++ wasify-frontend/pages/api/auth/[...auth0].js
@@ -0,0 +1,15 @@
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+
+const getLoginState = (req, loginOptions) => {
+  return { basket_id: getBasketId(req) };
+};
+
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, { getLoginState });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+});
