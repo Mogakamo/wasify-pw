0a75921c1724f96dab0e03ff0b1c9418c2394b0d
Mogakamo <mogaka.amo254@gmail.com>
2021-05-06 12:48:21 +0300

    commit

---
+++ wasify-frontend/lib/api-helpers.js
@@ -0,0 +1,10 @@
+//api-helpers
+const sensitiveFields = ["email", "emailVerified", "password"];
+export function extractUser(user) {
+  if (!user) return null;
+  const obj = {};
+  Object.keys(user).forEach((key) => {
+    if (!sensitiveFields.includes(key)) obj[key] = user[key];
+  });
+  return obj;
+}
