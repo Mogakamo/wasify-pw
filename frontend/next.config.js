7aea59ec1d3d05c5e2454fdd100dda12fb2bb831
Mogakamo <mogaka.amo254@gmail.com>
2021-06-06 15:07:55 +0300

    commit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3000"
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb+srv://Mogaka_dev:manyara5766@wasify-data.vss96.mongodb.net/Wasify?retryWrites=true&w=majority"
     }
 }
