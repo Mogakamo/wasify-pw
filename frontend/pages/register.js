7f700b874321cd797f1cee909c0061d2b166a8d4
Mogakamo <mogaka.amo254@gmail.com>
2021-06-06 15:26:50 +0300

    commit

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -14,6 +14,10 @@ const Register = () => {
 
                 <form className="mx-auto my-4" style={{ maxWidth: '500px' }}>
                     <div class="mb-3">
+                        <label htmlFor="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" />
+                    </div>
+                    <div class="mb-3">
                         <label htmlFor="exampleInputEmail1" class="form-label">Email address</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
