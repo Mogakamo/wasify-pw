4270455352038ffce59baf7512abb03e02f6f565
Mogakamo <mogaka.amo254@gmail.com>
2021-06-06 15:41:58 +0300

    commit

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import {useState} from 'react'
 
 
 const Register = () => {
+    const initialState = { name: '', email: '', password: '', cf_password: '' }
+    const [userData, setUserData] = useState(initialState)
+    const { name, email, password, cf_password } = userData
+
+    const handleChangeInput = e => {
+        const {name, value} = e.target
+        setUserData({...userData, [name]: value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(userData)
+    }
     return (
         <>
             <div>
@@ -12,23 +26,23 @@ const Register = () => {
                 </Head>
 
 
-                <form className="mx-auto my-4" style={{ maxWidth: '500px' }}>
+                <form className="mx-auto my-4" style={{ maxWidth: '500px' }} onSubmit={handleSubmit}>
                     <div class="mb-3">
                         <label htmlFor="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="name" />
+                        <input type="text" class="form-control" id="name" name="name" value={name} onChange={handleChangeInput} />
                     </div>
                     <div class="mb-3">
                         <label htmlFor="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={email } onChange={handleChangeInput} />
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-3">
                         <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type="password" class="form-control" id="exampleInputPassword1" name="password" value={password} onChange={handleChangeInput}/>
                     </div>
                     <div class="mb-3">
-                        <label htmlFor="exampleInputPassword2" class="form-label">Re-Enter Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword2" />
+                        <label htmlFor="exampleInputPassword2" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword2" name="cf_password" value={cf_password} onChange={handleChangeInput} />
                     </div>
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
