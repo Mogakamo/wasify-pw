842fcfd501d1c099b78e632307805dd2c2d84105
Mogakamo <mogaka.amo254@gmail.com>
2021-06-06 16:52:31 +0300

    commit

--- frontend/components/Notify.js
+++ frontend/components/Notify.js
@@ -1,5 +1,20 @@
-const Notify = () => {
+import { useContext } from 'react'
+import { DataContext } from '../store/GlobalState'
+import Loading from './Loading'
+import Toast from './Toast'
 
+
+const Notify = () => {
+    const [state, dispatch] = useContext(DataContext)
+    const { notify } = state
+
+    return (
+        <>
+            {notify.loading && <Loading />}
+            {notify.error && <Toast />}
+            {notify.success && <Toast />}
+        </>
+    )
 }
 
 export default Notify
