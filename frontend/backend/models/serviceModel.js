9a751135d0b83d7c3f3cb1ab8b76fa55ae2a45d8
Mogakamo <mogaka.amo254@gmail.com>
2021-06-14 17:23:07 +0300

    added the services model

---
+++ frontend/backend/models/serviceModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const serviceSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+let Dataset = mongoose.models.service || mongoose.model('service ', userSchema)
+export default Dataset
+
+//
