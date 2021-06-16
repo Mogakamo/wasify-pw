938fc26bcfa362c1633f6ea38c3e25ac815f8f0f
Mogakamo <mogaka.amo254@gmail.com>
2021-06-16 10:36:58 +0300

    commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,20 +1,27 @@
 import { getData } from '../backend/utils/fetchData'
+import { useState } from 'react'
 
-const Home = () => {
+const Home = (serviceProps) => {
+  //const [services, setServices] = useState(props.services)
+  
+
   return (
     <>
-      Home
+
     </>
   )
 }
 
 export async function getServerSideProps() {
 
-  const res = await getData('service');
-  console.log(res)
-  //server side rendering
+  // const res = await getData('service');
+  // console.log(res)
+  // //server side rendering
   return {
-    props: {}, // will be passed to the page component as props
+    props: {
+      // serviceProps: res.services,
+      // result: res.result
+    }, // will be passed to the page component as props
   }
 }
 
