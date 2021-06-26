5550be6efe8eed11c54760fd1ce0eaa6307cfd79
Mogakamo <mogaka.amo254@gmail.com>
2021-06-26 22:24:12 +0300

    commit

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { DataContext } from '../store/GlobalState'
 
 const Profile = () => {
@@ -10,23 +10,67 @@ const Profile = () => {
         cf_password: ''
     }
     const [data, setData] = useState(initialState)
-    const {avatar, name, password, cf_password} = data
+    const { avatar, name, password, cf_password } = data
 
     const { state, dispatch } = useContext(DataContext)
-    const { auth } = state
+    const { auth, notify } = state
+
+    useEffect(() => {
+       if (auth.user) setData({ ...data, name: auth.user.name })
+    }, [auth.user])
+
+    if (!auth.user) return null;
 
     return (
-        <div>
+        <div className="profile_page">
             <Head>
                 <title>Profile</title>
             </Head>
 
             <section className="row text-secondary">
-                <div className="col-md-4">
+                <div className="col-md-4 container-fluid">
                     <h3 className="text-center text-uppercase">{auth.user.role === 'user' ? 'User Profile' : 'Admin Profile'}</h3>
                     <div className="avatar">
                         <img src={auth.user.avatar} alt={auth.user.avatar} />
+                        <span>
+                            <i className="fas fa-camera"></i>
+                            <p>Change</p>
+                            <input type="file" name="file" id="file_up" />
+                        </span>
                     </div>
+
+                    <div className="form-group">
+                        <label htmlFor="name">
+                            Name
+                        </label>
+                        <input type="text" name="name" value={name} className="form-control"
+                            placeholder="Your name" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">
+                            Email
+                        </label>
+                        <input type="text" name="email" defaultValue={auth.user.email}
+                            className="form-control" placeholder="Email" disabled="true" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">
+                            New Password
+                        </label>
+                        <input type="text" name="password" value={password} className="form-control"
+                            placeholder="Your new password" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="cf_password">
+                            Confirm New Password
+                        </label>
+                        <input type="text" name="cf_password" value={cf_password} className="form-control"
+                            placeholder="Confirm new password" />
+                    </div>
+
+                    <button className="btn btn-info" disabled={notify.loading }>Update</button>
+
                 </div>
                 <div className="col-md-8">
                     <h3>Orders</h3>
